f37d73ef5d4c56bca876b045aa40528dbe3f1fce
damienlebourdonnec <damien.le-bourdonnec@groupe-regards.com>
2016-02-11 12:38:54 +0100

    Prepare version 0.3 for PyPI

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup
 setup(
     name = 'robotframework-websocketclient',
     packages = ['robotframework-websocketclient'],
-    version = '0.2',
+    version = '0.3',
     description = '	Robot Framework keywords for websocket-client',
     author = 'Damien Le Bourdonnec',
     author_email = 'damien.lebourdonnec@gmail.com',
     url = 'https://github.com/greums/robotframework-websocketclient',
-    download_url = 'https://github.com/greums/robotframework-websocketclient/tarball/0.1',
+    download_url = 'https://github.com/greums/robotframework-websocketclient/tarball/0.3',
     keywords = ['robotframework', 'websocket'],
     install_requires=[
         'websocket-client'

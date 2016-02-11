d5c2f5c61e345c723a86984016f54a36e7a872f2
damienlebourdonnec <damien.le-bourdonnec@groupe-regards.com>
2016-02-11 11:53:17 +0100

    Prepare PyPi import

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 '''
 '''
 from distutils.core import setup
@@ -32,3 +32,18 @@ def install():
 
 if __name__ == "__main__":
     install()
+
+from distutils.core import setup
+setup(
+  name = 'robotframework-websocketclient',
+  packages = ['robotframework-websocketclient'],
+  version = '0.1',
+  description = '	Robot Framework keywords for websocket-client',
+  author = 'Damien Le Bourdonnec',
+  author_email = 'damien.lebourdonnec@gmail.com',
+  url = 'https://github.com/greums/robotframework-websocketclient',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['robotframework', 'websocket'],
+  install_requires=['websocket-client'],
+  classifiers = [],
+)

a0dd5de89791ded240cddb5ac27b0f63e031f610
damienlebourdonnec <damien.le-bourdonnec@groupe-regards.com>
2016-02-11 12:19:54 +0100

    Import setup from setuptools instead of distutils for install_requires support

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
+
 setup(
-  name = 'robotframework-websocketclient',
-  packages = ['robotframework-websocketclient'],
-  version = '0.1',
-  description = '	Robot Framework keywords for websocket-client',
-  author = 'Damien Le Bourdonnec',
-  author_email = 'damien.lebourdonnec@gmail.com',
-  url = 'https://github.com/greums/robotframework-websocketclient',
-  download_url = 'https://github.com/greums/robotframework-websocketclient/tarball/0.1',
-  keywords = ['robotframework', 'websocket'],
-  install_requires=['websocket-client'],
-  classifiers = [],
+    name = 'robotframework-websocketclient',
+    packages = ['robotframework-websocketclient'],
+    version = '0.2',
+    description = '	Robot Framework keywords for websocket-client',
+    author = 'Damien Le Bourdonnec',
+    author_email = 'damien.lebourdonnec@gmail.com',
+    url = 'https://github.com/greums/robotframework-websocketclient',
+    download_url = 'https://github.com/greums/robotframework-websocketclient/tarball/0.1',
+    keywords = ['robotframework', 'websocket'],
+    install_requires=[
+        'websocket-client'
+    ],
+    classifiers = []
 )
